7f3fd60ebd0590ce2f059b084122a0a3e02b851f
Rodrigo Cavalcante <Rodrigo.Cavalcante@CSSP-567.local>
2017-01-12 18:11:13 -0200

    add decorator

---
+++ Design Patterns.playground/Pages/Decorator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+protocol Beverage {
+    func cost() -> Double
+    func description() -> String
+}
+
+class Coffe: Beverage {
+    func cost() -> Double {
+        return 0.95
+    }
+    
+    func description() -> String {
+        return "Coffe"
+    }
+}
+
+class Expresso: Beverage {
+    func cost() -> Double {
+        return 0.5
+    }
+    
+    func description() -> String {
+        return "Expresso"
+    }
+}
+
+class Mocha: Beverage {
+    let beverage: Beverage
+    
+    init(beverage: Beverage) {
+        self.beverage = beverage
+    }
+    
+    func cost() -> Double {
+        return 0.2 + self.beverage.cost()
+    }
+    
+    func description() -> String {
+        return self.beverage.description() + ", Mocha"
+    }
+}
+
+class Whip: Beverage {
+    let beverage: Beverage
+    
+    init(beverage: Beverage) {
+        self.beverage = beverage
+    }
+    
+    func cost() -> Double {
+        return 0.45 + self.beverage.cost()
+    }
+    
+    func description() -> String {
+        return self.beverage.description() + ", Whip"
+    }
+}
+
+var darkRoast: Beverage = Coffe()
+darkRoast = Mocha(beverage: darkRoast)
+darkRoast = Mocha(beverage: darkRoast)
+darkRoast = Whip(beverage: darkRoast)
+
+darkRoast.description()
+darkRoast.cost()
+
+var expresso: Beverage = Expresso()
+expresso.description()
+expresso.cost()
