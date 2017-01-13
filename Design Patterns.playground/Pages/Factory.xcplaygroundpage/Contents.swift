929dea973086b89483661ac1a9a1ebbc6fccddf0
Rodrigo Cavalcante <Rodrigo.Cavalcante@CSSP-567.local>
2017-01-13 11:38:48 -0200

    update factory

--- Design Patterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
+++ Design Patterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
@@ -12,25 +12,50 @@ protocol Pizza {
 }
 
 extension Pizza {
+    func prepare() {
+        
+    }
+    
     func bake() {
-        print("baking....")
+        print("Baking for 25 minutes at 150")
+    }
+    
+    func cut() {
+        print("Cutting the pizza into diagonal slices")
+    }
+    
+    func box() {
+        print("Place pizza in official PizzaStore box")
     }
 }
 
 class CheesePizza: Pizza {
-    
+    func prepare() {
+        print("Preparing Cheese Pizza")
+        print("Tossing dough...")
+        print("Adding cheese...")
+    }
 }
 
 class PepperoniPizza: Pizza {
-    
+    func prepare() {
+        print("Preparing Pepperoni Pizza")
+        print("Tossing dough...")
+        print("Adding cheese...")
+        print("Adding pepperoni...")
+    }
 }
 
 class VeggiePizza: Pizza {
-    
+    func prepare() {
+        print("Preparing Veggie Pizza")
+        print("Tossing dough...")
+        print("Adding cheese...")
+        print("Adding veggies...")
+    }
 }
 
 class PizzaFactory {
-    
     func createPizza(flavor: Flavor) -> Pizza {
         switch flavor {
         case .cheese:
@@ -39,8 +64,25 @@ class PizzaFactory {
             return PepperoniPizza()
         case .veggie:
             return VeggiePizza()
+        }
     }
 }
 
-let factory = PizzaFactory()
-let pepperoni = factory.
+
+class PizzaStore {
+    let pizzaFactory = PizzaFactory()
+    
+    func orderPizza(flavor: Flavor) {
+        let pizza = pizzaFactory.createPizza(flavor: flavor)
+        
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+    }
+}
+
+let pizzaStore = PizzaStore()
+pizzaStore.orderPizza(flavor: .cheese)
+pizzaStore.orderPizza(flavor: .pepperoni)
+pizzaStore.orderPizza(flavor: .veggie)
